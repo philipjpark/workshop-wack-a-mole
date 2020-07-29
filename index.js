fda43991cec5b1208bfb04242e5a2f21231d117e
philipjpark <66290921+philipjpark@users.noreply.github.com>
2020-07-29 12:12:50 -0400

    finished counter; added purple background :)

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 let score = 0;
+let display = document.getElementById('score');
+
+let holes = Array.from(document.getElementsByClassName("hole"));
+
+setInterval(function() {
+    var randomHoleIndex = Math.floor((Math.random() * holes.length));
+    holes[randomHoleIndex].classList.toggle("mole");
+}, 900);
+
+const game = document.getElementById("whack-a-mole");
+
+game.addEventListener('click', function (evt) {
+   if(evt.target.matches(".mole")) {
+       score++;
+       display.innerText = score;
+       var click = holes.indexOf(evt.target);
+       holes[click].classList.toggle("mole",false);
+   }
+});
